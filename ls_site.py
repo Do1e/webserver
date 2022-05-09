64067cad3fcbffca5b25bbf3f03c4a063a5ba3bd
do1e <dpj.email@qq.com>
2022-05-09 22:34:52 +0800

    fix ls_site

--- ls_site.py
+++ ls_site.py
@@ -1,5 +1,4 @@
 import os
-rootPath = "./site"
 
 class lsSite():
 	def __init__(self, path):
@@ -13,7 +12,7 @@ class lsSite():
 			path += "/"
 		fpw = open(path + "index.html", "wb")
 		fpw.write("<html><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /><head><title>index</title></head><body><ul>".encode("utf-8"))
-		if(path != rootPath and path != rootPath + "/"):
+		if(depth != 0):
 			fpw.write("\n<p style=\"margin: 25px;\"><li><a href=\"../\" style=\"font-size:25px\">dir: ../</a></li></p>".encode("utf-8"))
 		for item in os.listdir(path):
 			if(item == "404.html" \
@@ -23,25 +22,17 @@ class lsSite():
 			itemPath = os.path.join(path, item)
 			if(os.path.isdir(itemPath)):
 				self.listdir_recursive(itemPath, depth + 1)
-				Str = "\n<p style=\"margin: 25px;\"><li><a href=\""
-				if(depth == 0):
-					Str += "./"
-				else:
-					for i in range(depth):
-						Str += "../"
-				fpw.write((Str + itemPath[7:] + "/index.html\" style=\"font-size:25px\">dir: " + itemPath[7:] + "/</a></li></p>").encode("utf-8"))
+			Str = "\n<p style=\"margin: 25px;\"><li><a href=\""
+			if(depth == 0):
+				Str += "./"
 			else:
-				Str = "\n<p style=\"margin: 25px;\"><li><a href=\""
-				if(depth == 0):
-					Str += "./"
-				else:
-					for i in range(depth):
-						Str += "../"
-				fpw.write((Str + itemPath[7:] + "\" style=\"font-size:25px\">" + itemPath[7:] + "</a></li></p>" ).encode("utf-8"))
+				for i in range(depth):
+					Str += "../"
+			fpw.write((Str + itemPath[self.len+1:] + "\" style=\"font-size:25px\">").encode("utf-8"))
+			if(os.path.isdir(itemPath)):
+				fpw.write("dir: ".encode("utf-8"))
+			fpw.write((itemPath[self.len+1:] + "</a></li></p>").encode("utf-8"))
 		fpw.write("\n</ul></body></html>".encode("utf-8"))
 		fpw.close()
 	def run(self):
 		self.listdir_recursive(self.path, 0)
-
-ls = lsSite(rootPath)
-ls.run()
